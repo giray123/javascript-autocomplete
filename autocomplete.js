a80890921fb03576a4a20dbaf4f6cfc82996e1fc
giray songul <giray123@gmail.com>
2020-10-28 11:05:17 +0300

    seperated autocomplete.css from javascript

--- autocomplete.js
+++ autocomplete.js
@@ -1,4 +1,4 @@
-var Autocomplete = function({selector, minChar, extraParentClasses, ajax, manualAjax, submitHandler, delay, highlight}){
+var Autocomplete = function({selector, minChar, extraParentClasses, ajax, fixSearch, manualAjax, submitHandler, delay, highlight}){
     // variable declerations
     var processId   = null,
         $input      = document.querySelector(selector),
@@ -9,10 +9,12 @@ var Autocomplete = function({selector, minChar, extraParentClasses, ajax, manual
         highlight = typeof highlight  == "undefined" ? true : highlight,
         url;
 
-    // create a wrapper div for input and suggestion list
+    // create a wrapper
     var $parent = $input.parentNode;
     var $wrapper = document.createElement('div');
+        $wrapper.classList.add("ac-wrapper")
         $wrapper.style.position = "relative";
+        $wrapper.style.display = "inline-block";
         if(extraParentClasses){
             $wrapper.className+=' '+extraParentClasses;
         }
@@ -22,11 +24,6 @@ var Autocomplete = function({selector, minChar, extraParentClasses, ajax, manual
     // create a clear icon
     var $icon_clear = document.createElement('div');
         $icon_clear.classList.add("ac-autocomplete-clear-icon", "ac-hidden")
-        $icon_clear.innerHTML = `
-            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"  width="24" height="24" viewBox="0 0 24 24">
-                <path fill="#969696" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
-            </svg>
-        `
         $icon_clear.title = "clear"
         $icon_clear.addEventListener('click', iconClearClick)
         $wrapper.appendChild($icon_clear);
@@ -41,107 +38,24 @@ var Autocomplete = function({selector, minChar, extraParentClasses, ajax, manual
 
     // create suggestion list
     var $list = document.createElement('div')
-                $list.classList.add('ac-suggestion_list')
-                $wrapper.appendChild($list);
+        $list.classList.add('ac-suggestion-list')
+        $wrapper.appendChild($list);
 
-    $input.style.paddingRight = "50px"
-    var inputPaddingLeft = 0
-    function syncCssParameters(){
-        var inputStyles = getComputedStyle($input)
-        $list.style.fontFamily = inputStyles.fontFamily
-        $list.style.fontSize = inputStyles.fontSize
-        $list.style.lineHeight = inputStyles.lineHeight
-        inputPaddingLeft = inputStyles.paddingLeft
-        inputHeight = inputStyles.height
-    }
-    function createEmbeddedCSS(){
-        var styles = `
-            // AUTOCOMPLETE CSS - start
-            .ac-hidden{
-                display: none;
-            }
-            .ac-suggestion_list{ 
-                width: 100%;
-                position: absolute;
-                left: 0px;
-                text-align: left;
-                background-color: white;
-                max-width: 100%;
-                z-index: 5;
-                border-radius: 5px;
-                overflow: hidden;
-                border: 1px solid #e8e8e8;
-                border-top: none;
-            }
-            .ac-autocomplete-clear-icon{
-                position: absolute;
-                top: 0;
-                right: 35px; 
-                width: 40px;
-                height: 100%;
-            }
-            .ac-autocomplete-clear-icon:hover{
-                cursor: pointer;
-            }
-            .ac-suggestion_list div.autocomplete-result { 
-                max-width: 100%;
-                display: flex;
-            }
-            .ac-suggestion_list div:hover { 
-                cursor: pointer;
-                background-color: whitesmoke;
-            }
-            .ac-suggestion_list div.active { 
-                background-color: #e4e2e2;
-            }
-            .ac-suggestion_list div.autocomplete-text { 
-                padding: 5px ${inputPaddingLeft};
-                white-space: nowrap;
-                overflow: hidden;
-                text-overflow: ellipsis;
-                flex-grow: 1;
-            }
-            .ac-suggestion_list div.autocomplete-mobile-icon { 
-                display: none;
-                flex-shrink: 0;
-                width: 40px;
-                background-image: url(/public/img/icons/arrow-top-left.svg);
-                background-repeat: no-repeat;
-                background-position: center;
-            }
-            @media screen and (max-width: 768px) {
-                .ac-suggestion_list div.autocomplete-result { 
-                    min-height: 40px;
-                }
-                .ac-suggestion_list div.autocomplete-mobile-icon { 
-                    display: initial;
-                }
-            }
-            // AUTOCOMPLETE CSS - end
-        `
-        var styleSheet = document.createElement("style")
-        styleSheet.innerText = styles
-        document.head.appendChild(styleSheet)
-    }
-
-    syncCssParameters()
-    createEmbeddedCSS()
+    // input event listeners
     $input.addEventListener('input', inputChange)
     $input.addEventListener('keydown', keydown)
     $input.addEventListener('blur', blur)
 
-
     function blur(){
-        // console.log('blur')
         cancelAjaxSearch()
         // $list.innerHTML = "";
     }
+
     function inputChange(){
-        // console.log('inputChange')
         cancelAjaxSearch()
         input_backup = $input.value
-        if($input.value != ""){
-            processId = setTimeout(()=>{search()}, delay)
+        if(input_backup != ""){
+            processId = setTimeout(()=>{startAjaxSearch(input_backup)}, delay)
         }
         checkIconClear()
     }
@@ -217,20 +131,14 @@ var Autocomplete = function({selector, minChar, extraParentClasses, ajax, manual
             // do nothing
         }
     }
-    // function scrollList(parent, child){
-    //     if(child.offsetTop > (parent.offsetHeight - child.offsetHeight)){
-    //         parent.scrollTop = child.offsetTop - (parent.offsetHeight - child.offsetHeight);
-    //     }
-    // }
     function cancelAjaxSearch(){
         clearTimeout(processId)
     }
 
-    async function search(input){
-        // console.log('search')
-        var search = $input.value.toLowerCase()
-        // console.log('search:' + search)
-        if(search.length < minChar) return;
+    async function startAjaxSearch(search){
+        if(search.length < minChar) return
+
+        if(fixSearch){ search = fixSearch(search)}
 
         var results = []
         if(manualAjax){
@@ -292,8 +200,6 @@ var Autocomplete = function({selector, minChar, extraParentClasses, ajax, manual
                         $mobile_icon.setAttribute("data", i)
                         $mobile_icon.addEventListener('mousedown', resultIconClick)
                         $result.appendChild($mobile_icon)
-                    
-                    
             }
         }
         // reset active index
@@ -302,6 +208,7 @@ var Autocomplete = function({selector, minChar, extraParentClasses, ajax, manual
 
     function resultClick(e){
         e.preventDefault()
+        if(e.which != 1) return; // only progress on left mouse clicks
         console.log('resultClick')
         $input.value = e.currentTarget.innerText
         $list.innerHTML = ""
@@ -312,7 +219,7 @@ var Autocomplete = function({selector, minChar, extraParentClasses, ajax, manual
         console.log('iconClick')
         $input.value = $list.childNodes[e.currentTarget.getAttribute("data")].innerText
         cancelAjaxSearch()
-        search()
+        startAjaxSearch($input.value)
         $input.focus()
     }
     function value(str){
@@ -332,7 +239,6 @@ var Autocomplete = function({selector, minChar, extraParentClasses, ajax, manual
                 activeIndex : activeIndex
             }
         },
-        search: search,
         focus: focus,
         value: value
     }
